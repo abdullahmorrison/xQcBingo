713e35ffcfe2a7a977bf8698550c6b3398c0d6fd
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-10-08 12:00:01 -0400

    completed logic for adding a chip to a quadrant

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
+/**
+ * Inserts bingo chips in the quadrant clicked
+ */
 $(".quadrant").click(function (e) {
-    if($(e.target).find('.chip').length > 0){
-        $(".chip").remove();
-    }else{
+    if($(e.target).is($(".quadrant")) 
+        && $(e.target).find('.chip').length > 0){//removes chip from quadrant if you click a quadrant
+        $(e.target).find('.chip').remove();
+    }else if($(e.target).parent().is($(".quadrant")) 
+        && $(e.target).parent().find('.chip').length > 0){//removes chip from quadrant if you click an element in a quadrant
+        $(e.target).parent().find('.chip').remove();
+    }else if($(e.target).parent().is($(".quadrant")) 
+        && $(e.target).parent().find('.chip').length == 0){//adds chip from quadrant if you click a quadrant
+        $(e.target).parent().prepend('<div class="chip" draggable="true">Chippy Chip</div>');
+    }else{//adds chip from quadrant if you click an element in a quadrant
         $(e.target).prepend('<div class="chip" draggable="true">Chippy Chip</div>');
     }
 });
