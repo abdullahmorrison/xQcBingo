5515735c8880e357f267f06ce0034b31dbaf7efc
Abdullah Morrison <abdullahmorrison@gmail.com>
2020-10-08 17:50:32 -0400

    completed bingo win logic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-/**
+var chipsInARow = 3;
+/*
  * Inserts bingo chips in the quadrant clicked
  */
 $(".quadrant").click(function (e) {
@@ -11,7 +12,67 @@ $(".quadrant").click(function (e) {
     }else if($(e.target).parent().is($(".quadrant")) 
         && $(e.target).parent().find('.chip').length == 0){//adds chip from quadrant if you click a quadrant
         $(e.target).parent().prepend('<div class="chip" draggable="true">Chippy Chip</div>');
+        if(checkWin(chipsInARow)){
+            alert('BINGO!');
+        }
     }else{//adds chip from quadrant if you click an element in a quadrant
         $(e.target).prepend('<div class="chip" draggable="true">Chippy Chip</div>');
+        if(checkWin(chipsInARow)){
+            alert('BINGO!');
+        }
     }
 });
+
+function checkWin(chipsInARow){
+    var chips = 0;
+    const quadrants = document.querySelectorAll(".quadrant");
+    //*Check horizontal win
+    for(var i=0; i<quadrants.length; i=i+chipsInARow){//looking only the left quadrants first
+        if($(quadrants[i]).find('.chip').length > 0){
+            for(j = i; j<i+chipsInARow; j++){ //checking the row
+                if($(quadrants[j]).find('.chip').length > 0){
+                    chips++; 
+                }
+            }
+            if(chips == chipsInARow){//checking for horizonal win
+                return true;
+            }
+        }
+        chips = 0;
+    }
+    //*Check vertical win
+    for(var i=0; i<chipsInARow; i++){//looking only at the top quadrants first
+        if($(quadrants[i]).find('.chip').length > 0){
+            for(j = i; j<quadrants.length; j=j+chipsInARow){//checking the column
+                if($(quadrants[j]).find('.chip').length > 0){
+                    chips++;
+                }
+            }
+            if(chips == chipsInARow){//checking for vertical win
+                return true;
+            }
+        }
+        chips = 0;
+    }
+    //*Check diagonal win
+    if($(quadrants[0]).find('.chip').length > 0){//checking the top left corner
+        for(j = 0; j<quadrants.length; j=j+chipsInARow+1){//looking at the right diagonal
+            if($(quadrants[j]).find('.chip').length > 0){
+                chips++;
+            }
+        } 
+        if(chips == chipsInARow){//checking for diagonal win
+            return true;
+        }
+    }else if($(quadrants[chipsInARow-1]).find('.chip').length > 0){//checking the top right corner
+        for(j = chipsInARow-1; j<quadrants.length; j=j+chipsInARow-1){//looking at the left diagonal
+            if($(quadrants[j]).find('.chip').length > 0){
+                chips++;
+            }
+        }  
+        if(chips == chipsInARow){//checking for diagonal win
+            return true;
+        }  
+    }
+    return false;
+}
